9d8eaf4b49b2ef5a265f87ea2444d68db094869c
williamsconcepts <chidiekwebelam@gmail.com>
2025-11-23 11:52:44 +0100

    fixed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2025: true },
+  env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
@@ -12,9 +12,6 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
+     "react/prop-types": "off",
   },
 };
--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,23 +1,8 @@
-import styles from "./City.module.css";
 
-const formatDate = (date) =>
-  new Intl.DateTimeFormat("en", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    weekday: "long",
-  }).format(new Date(date));
 
 function City() {
   // TEMP DATA
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
 
-  const { cityName, emoji, date, notes } = currentCity;
 
   return <h1>City</h1>;
 
